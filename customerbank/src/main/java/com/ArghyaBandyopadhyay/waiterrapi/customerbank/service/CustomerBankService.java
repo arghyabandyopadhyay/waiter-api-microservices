ebaeef23584d0e4b6bad623761a08bcac70cdc5d
Arghya Bandyopadhyay <72153788+arghyabandyopadhyay@users.noreply.github.com>
2023-02-24 23:08:29 +0530

    Create CustomerBankService.java

---
+++ customerbank/src/main/java/com/ArghyaBandyopadhyay/waiterrapi/customerbank/service/CustomerBankService.java
@@ -0,0 +1,18 @@
+package com.ArghyaBandyopadhyay.waiterrapi.customerbank.service;
+
+
+import com.ArghyaBandyopadhyay.waiterrapi.customerbank.model.CustomerDetails;
+import com.ArghyaBandyopadhyay.waiterrapi.customerbank.util.exception.CustomerAlreadyExistsException;
+import com.ArghyaBandyopadhyay.waiterrapi.customerbank.util.exception.CustomerNotExistsException;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface CustomerBankService {
+
+    public CustomerDetails addCustomerDetail(CustomerDetails customerDetails) throws CustomerAlreadyExistsException;
+    public void updateCustomerDetail(CustomerDetails customerDetails) throws CustomerNotExistsException;
+    public void deleteCustomerDetail(UUID id) throws CustomerNotExistsException;
+    public CustomerDetails getCustomerDetail(UUID id) throws CustomerNotExistsException;
+    public CustomerDetails getCustomerDetailUsingMobileNo(String mobileNo)throws CustomerNotExistsException;
+}
