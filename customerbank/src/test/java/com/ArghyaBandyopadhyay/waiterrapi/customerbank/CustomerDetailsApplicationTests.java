ce7cb2a489fd022c00ee62e003b0abc0fabf8f86
Arghya Bandyopadhyay <72153788+arghyabandyopadhyay@users.noreply.github.com>
2023-02-24 23:09:32 +0530

    Create CustomerDetailsApplicationTests.java

---
+++ customerbank/src/test/java/com/ArghyaBandyopadhyay/waiterrapi/customerbank/CustomerDetailsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ArghyaBandyopadhyay.waiterrapi.customerbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerDetailsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
