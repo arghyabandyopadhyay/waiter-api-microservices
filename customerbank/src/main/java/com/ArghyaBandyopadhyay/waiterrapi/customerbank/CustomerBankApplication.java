42632681dcddccbe4f906e39c476207db4fb11c9
Arghya Bandyopadhyay <arghyab13bga@gmail.com>
2023-02-25 15:39:05 +0530

    Update CustomerBankApplication.java

--- customerbank/src/main/java/com/ArghyaBandyopadhyay/waiterrapi/customerbank/CustomerBankApplication.java
+++ customerbank/src/main/java/com/ArghyaBandyopadhyay/waiterrapi/customerbank/CustomerBankApplication.java
@@ -3,12 +3,11 @@ package com.ArghyaBandyopadhyay.waiterrapi.customerbank;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@EnableEurekaClient
+//@EnableEurekaClient
 public class CustomerBankApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CustomerBankApplication.class, args);
