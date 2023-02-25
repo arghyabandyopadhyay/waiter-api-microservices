5335b5403e139fe82bf4880157270ed54849f753
Arghya Bandyopadhyay <arghyab13bga@gmail.com>
2023-02-25 15:39:53 +0530

    Create TransactionAlreadyExistsException.java

---
+++ transactionservice/src/main/java/com/ArghyaBandyopadhyay/waiterrapi/commentforkotsuggestion/util/exception/TransactionAlreadyExistsException.java
@@ -0,0 +1,11 @@
+package com.ArghyaBandyopadhyay.waiterrapi.commentforkotsuggestion.util.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+@ResponseStatus(code = HttpStatus.CONFLICT,reason = "Transaction already exists")
+public class TransactionAlreadyExistsException extends Exception{
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+}
