7ac07e4d6adf4100563083a837c84131b005d7a9
Arghya Bandyopadhyay <arghyab13bga@gmail.com>
2023-02-25 15:38:54 +0530

    Create CommentForKOTSuggestionApplicationTests.java

---
+++ commentforkotsuggestion/src/test/java/com/ArghyaBandyopadhyay/waiterrapi/commentforkotsuggestion/CommentForKOTSuggestionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ArghyaBandyopadhyay.waiterrapi.commentforkotsuggestion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommentForKOTSuggestionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
