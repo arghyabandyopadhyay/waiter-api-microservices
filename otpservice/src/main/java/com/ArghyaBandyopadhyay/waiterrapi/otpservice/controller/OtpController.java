155c0c21f8972dcf23b463cef2b3af0717227944
Arghya Bandyopadhyay <arghyab13bga@gmail.com>
2023-02-25 15:39:23 +0530

    Update OtpController.java

--- otpservice/src/main/java/com/ArghyaBandyopadhyay/waiterrapi/otpservice/controller/OtpController.java
+++ otpservice/src/main/java/com/ArghyaBandyopadhyay/waiterrapi/otpservice/controller/OtpController.java
@@ -14,7 +14,7 @@ import java.util.Map;
 
 @RestController
 //@CrossOrigin(origins = "http://localhost:4200/")
-@RequestMapping("api/v1/otp")
+@RequestMapping("api/v1/waiterr/otp")
 public class OtpController {
     /*
      * Autowiring should be implemented for the NewsService. Please note that we
